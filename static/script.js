f97c6bb13145ff87d6571608c7721fcbcbcd8c91
rostok2 <iross335@gmail.com>
2024-07-20 15:36:21 +0300

    add code for faq-item

--- static/script.js
+++ static/script.js
@@ -72,3 +72,20 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function toggleAnswer(element) {
+    const answer = element.nextElementSibling;
+    const icon = element.querySelector('.icon');
+
+    if (answer.style.height && answer.style.height !== '0px') {
+        // Згорнути відповідь
+        answer.style.height = '0';
+        answer.style.opacity = '0';
+        icon.textContent = "+";
+    } else {
+        // Відкрити відповідь
+        answer.style.height = answer.scrollHeight + 'px';
+        answer.style.opacity = '1';
+        icon.textContent = "−";
+    }
+}
